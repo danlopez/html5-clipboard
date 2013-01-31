27d2443859981792abdc89b554ab26189d082f0a
Dan nginx <dan@lessonsmith.com>
2013-01-31 07:29:20 +0000

    Tried installing the ACE WYSIWYG Editor.  +200Kb.  Too large.  Tried converting the editable div to textarea.  Fixed the empty element bug, but removed all rich text.  Reverted both

--- js/main.js
+++ js/main.js
@@ -1,30 +1,46 @@
 $(function() {
 
-    $('#editable').html(localStorage.getItem("myClipboard")).focus();
-    var saveTimer, lastSaveTime = 0;
-    var minSaveTime = 1000;
+    function supports_html5_storage() {
+        try {
+            return 'localStorage' in window && window['localStorage'] !== null;
+        } catch (e) {
+            return false;
+        }
+    }
 
-    $('#editable').on('input', function() {
-   
-        var now = new Date().getTime();
-    
+    if(supports_html5_storage()){
+
+        $('#editable').html(localStorage.getItem("myClipboard")).focus();
+       
+        var saveTimer, lastSaveTime = 0;
+        var minSaveTime = 1000;
+
         function saveProgress() {
-            $('#worksave').show().html("Work saved").fadeOut();
-            localStorage.setItem("myClipboard", $('#editable').html());
+            $('#worksave').show().html("Saving...").fadeOut('slow');
+            try {
+                localStorage.setItem("myClipboard", $('#editable').html());
+            } catch (e) {
+                alert("Current Save Operation failed.  Local storage capacity reached");
+            }
         }
 
-        if (!saveTimer) {
-            if (now - lastSaveTime > (3 * minSaveTime)) {
-                saveProgress();   // fire immediately on first scroll
-                lastSaveTime = now;
-            }
+        $('#editable').on('input', function() {
+   
+            var now = new Date().getTime();
+    
+            if (!saveTimer) {
+                if (now - lastSaveTime > (3 * minSaveTime)) {
+                    saveProgress();   // fire immediately on first scroll
+                    lastSaveTime = now;
+                }
         
-            saveTimer = setTimeout(function() {
-                saveTimer = null;
-                lastSaveTime = new Date().getTime();
-                saveProgress();
-            }, minSaveTime);
-        }
-    });
+                saveTimer = setTimeout(function() {
+                    saveTimer = null;
+                    lastSaveTime = new Date().getTime();
+                    saveProgress();
+                }, minSaveTime);
+            }
+        });
+    }
 
 });
